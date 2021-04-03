018b52dbbe6733188dfbecbd9936d8025f52e637
DELL <DELL@DESKTOP-LAPNUH5>
2021-04-03 13:53:10 -0600

    Corregido

--- ProyectoCruceros/src/Clases/PuertosDestinos.java
+++ ProyectoCruceros/src/Clases/PuertosDestinos.java
@@ -10,8 +10,7 @@ public class PuertosDestinos {
     private String Puerto;
     private String Estado;
     private int IdDestino;
-    private int IdPuerto;
-
+    
     public int getIdDestino() {
         return IdDestino;
     }
@@ -36,25 +35,16 @@ public class PuertosDestinos {
         this.Puerto = Puerto;
     }
 
-    public int getIdPuerto() {
-        return IdPuerto;
-    }
-
-    public void setIdPuerto(int IdPuerto) {
-        this.IdPuerto = IdPuerto;
-    }
-
     public void ingresar(){
         
         try{
         Connection con = ClsConexion.obtenerConexion();
         PreparedStatement ps = con.prepareStatement("INSERT INTO "
-                + "[dbo].[Puertos-Destinos] ([codigo_puerto],[descripcion],"
-                + "[codigo_destino],[estado]) VALUES (?,?,?,?)");
-        ps.setInt(1, getIdPuerto());
-        ps.setString(2,getPuerto());
-        ps.setInt(3,getIdDestino());
-        ps.setString(4,getEstado()); 
+                + "[dbo].[Puertos-Destinos] ([descripcion],"
+                + "[codigo_destino],[estado]) VALUES (?,?,?)");
+        ps.setString(1,getPuerto());
+        ps.setInt(2,getIdDestino());
+        ps.setString(3,getEstado()); 
         ps.executeUpdate();
         JOptionPane.showMessageDialog(null, "Registro Ingresado");
         
@@ -72,9 +62,8 @@ public class PuertosDestinos {
         try{
         Connection con = ClsConexion.obtenerConexion();
         PreparedStatement ps = con.prepareStatement("INSERT INTO "
-                + "[dbo].[Puertos-Destinos] ([codigo_puerto],[descripcion],"
+                + "[dbo].[Puertos-Destinos] ([descripcion],"
                 + "[codigo_destino],[estado]) VALUES (?,?,?,?)");
-        ps.setInt(1, getIdPuerto());
         ps.setString(2,getPuerto());
         ps.setInt(3,getIdDestino());
         ps.setString(4,getEstado()); 
